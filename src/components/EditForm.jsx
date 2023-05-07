07b9f2e68dddc743818c62ce8197d2a96b89f73f
Mangaliso Qoshe <michael.qoshe98@gmail.com>
2023-05-07 14:46:32 +0200

    add css styles and implement eventlister for modal component

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import { CheckIcon } from "@heroicons/react/24/solid";
+import styles from "./Editform.module.css";
 
-
-export default function EditForm({editedTask,updateTask}) {
+export default function EditForm({ editedTask, updateTask }) {
   const [updateValue, setUpdatevalue] = useState(editedTask.name);
 
-const handleFormSubmit=(e)=>{
-    e.preventDefault()
-    updateTask({...editedTask,name:updateValue})
-    
-}
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    updateTask({ ...editedTask, name: updateValue });
+  };
 
   return (
-    <div role="dialog">
-      <form onSubmit={handleFormSubmit}>
+    <>
+      <div role="dialog" />
+      <form onSubmit={handleFormSubmit} className={styles["edit-form"]}>
         <input
           type="text"
+          className={styles["update-task"]}
           autoFocus
           maxLength={60}
           placeholder="Update Task"
@@ -26,9 +27,13 @@ const handleFormSubmit=(e)=>{
             setUpdatevalue(e.target.value);
           }}
         />
-        <label htmlFor="editedTask">Update Value</label>
-        <button type="submit"><CheckIcon strokeWidth={2} height={32} width={32}/></button>
+        <label htmlFor="editedTask" className={styles.label}>
+          Update Task
+        </label>
+        <button className={styles.btn} type="submit">
+          <CheckIcon strokeWidth={2} height={32} width={32} />
+        </button>
       </form>
-    </div>
+    </>
   );
 }
